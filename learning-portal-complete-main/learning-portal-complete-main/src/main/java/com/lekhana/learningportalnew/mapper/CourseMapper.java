a946daa4d31e700785103a0d899d5c13a7d49195
Lekhanaa7 <158190793+Lekhanaa7@users.noreply.github.com>
2024-02-17 09:21:35 +0530

    created a learning portal similar to udemy

---
+++ learning-portal-complete-main/learning-portal-complete-main/src/main/java/com/lekhana/learningportalnew/mapper/CourseMapper.java
@@ -0,0 +1,28 @@
+package com.lekhana.learningportalnew.mapper;
+
+import com.lekhana.learningportalnew.dto.CourseDTO;
+import com.lekhana.learningportalnew.entity.CourseEntity;
+
+public class CourseMapper {
+
+	public static CourseEntity populateCourse(CourseDTO courseDTO) {
+		CourseEntity courseEntity = new CourseEntity();
+		courseEntity.setCourseId(courseDTO.getCourseId());
+		courseEntity.setTitle(courseDTO.getTitle());
+		courseEntity.setDescription(courseDTO.getDescription());
+		courseEntity.setPrice(courseDTO.getPrice());
+		courseEntity.setCategory(courseDTO.getCategory());
+		return courseEntity;
+	}
+
+	public static CourseDTO courseEntityToDTO(CourseEntity courseEntity) {
+		CourseDTO courseDTO = new CourseDTO();
+		courseDTO.setCourseId(courseEntity.getCourseId());
+		courseDTO.setTitle(courseEntity.getTitle());
+		courseDTO.setDescription(courseEntity.getDescription());
+		courseDTO.setPrice(courseEntity.getPrice());
+		courseDTO.setCategory(courseEntity.getCategory());
+		return courseDTO;
+	}
+
+}
