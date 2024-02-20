60caf995e2bb94dc0f94038465f7798912e00379
Lekhanaa7 <158190793+Lekhanaa7@users.noreply.github.com>
2024-02-20 21:58:03 +0530

    created a learning portal similar to udemy

---
+++ LearningPortal/learning-portal-complete-main/learning-portal-complete-main/src/main/java/com/lekhana/learningportalnew/mapper/RegisteredMapper.java
@@ -0,0 +1,24 @@
+package com.lekhana.learningportalnew.mapper;
+
+import com.lekhana.learningportalnew.dto.RegisteredCourseDTO;
+import com.lekhana.learningportalnew.entity.RegisteredCourseEntity;
+
+public class RegisteredMapper {
+
+	public static RegisteredCourseEntity populateRegisteredCourse(RegisteredCourseDTO registeredCourseDTO) {
+		RegisteredCourseEntity registeredCourseEntity = new RegisteredCourseEntity();
+		registeredCourseEntity.setRegistrationId(registeredCourseDTO.getRegistrationId());
+		registeredCourseEntity.setUser(registeredCourseDTO.getUser());
+		registeredCourseEntity.setCourse(registeredCourseDTO.getCourse());
+		return registeredCourseEntity;
+	}
+
+	public static RegisteredCourseDTO regCourseEntitytoDTO(RegisteredCourseEntity registeredCourseEntity) {
+		RegisteredCourseDTO registeredCourseDTO = new RegisteredCourseDTO();
+		registeredCourseDTO.setRegistrationId(registeredCourseEntity.getRegistrationId());
+		registeredCourseDTO.setUser(registeredCourseEntity.getUser());
+		registeredCourseDTO.setCourse(registeredCourseEntity.getCourse());
+		return registeredCourseDTO;
+	}
+
+}
---
+++ LearningPortal/learning-portal-complete-main/learning-portal-complete-main/src/test/java/com/lekhana/learningportalnew/LearningPortalNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lekhana.learningportalnew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningPortalNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
