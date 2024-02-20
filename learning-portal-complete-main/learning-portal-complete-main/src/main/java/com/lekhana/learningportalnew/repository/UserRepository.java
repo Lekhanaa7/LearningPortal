2dc4b5c0517ebff30b36ea5c27ac75cfcba8eb16
Lekhanaa7 <158190793+Lekhanaa7@users.noreply.github.com>
2024-02-20 21:53:32 +0530

    Delete learning-portal-complete-main/learning-portal-complete-main directory

--- learning-portal-complete-main/learning-portal-complete-main/src/main/java/com/lekhana/learningportalnew/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lekhana.learningportalnew.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.lekhana.learningportalnew.entity.UserEntity;
-
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
-
-}
